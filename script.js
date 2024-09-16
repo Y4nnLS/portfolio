fdb5f7dcba9d4ef219f5a7a6b95741c795ef1c7a
Yann Lucas <101208372+Y4nnLS@users.noreply.github.com>
2024-09-16 16:58:08 -0300

    atualização do portfolio

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-$('.portfolio-button').mousedown(function(){
-    timeout = setInterval(function(){
-        window.scrollBy(0,-1); // May need to be -1 to go down
-    }, 0); // Play around with this number. May go too fast
+// jQuery(document).ready(function(){
+//     jQuery('.portfolio-button').mousedown(function(){
+//         timeout = setInterval(function(){
+//             window.scrollBy(0,-1); // May need to be -1 to go down
+//         }, 0); // Play around with this number. May go too fast
 
-    return false;
-});
+//         return false;
+//     });
+// });
